3de02c6e633ed3a178fdd0ee936434f0a6636864
SpookyScaryDev <thomaslcave@gmail.com>
2022-08-18 01:45:10 -0700

    Added premake binaries

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -4,8 +4,8 @@ echo Building NEA project files.
 echo.
 
 @echo on
-call premake5.exe vs2019
-call premake5.exe export-compile-commands
+call premake5\premake5.exe vs2019
+call premake5\premake5.exe export-compile-commands
 
 copy compile_commands\debug.json compile_commands.json
 @echo off
