ceb9b555e6cf6fcbf8194ff207fdf68102f6b9d3
SpookyScaryDev <thomaslcave@gmail.com>
2022-08-30 01:46:14 -0700

    Moved Vendor folder

--- Prototype/premake5.lua
+++ Prototype/premake5.lua
@@ -11,28 +11,28 @@ project "Prototype"
     objdir     (projectdir .. "Intermediate/" .. outputdir)
 
     files {
-        projectdir .. "Vendor/imgui/backends/imgui_impl_sdl.h",
-        projectdir .. "Vendor/imgui/backends/imgui_impl_sdl.cpp",
+        projectdir .. "../Vendor/imgui/backends/imgui_impl_sdl.h",
+        projectdir .. "../Vendor/imgui/backends/imgui_impl_sdl.cpp",
 
-        projectdir .. "Vendor/imgui/backends/imgui_impl_sdlrenderer.h",
-        projectdir .. "Vendor/imgui/backends/imgui_impl_sdlrenderer.cpp",
+        projectdir .. "../Vendor/imgui/backends/imgui_impl_sdlrenderer.h",
+        projectdir .. "../Vendor/imgui/backends/imgui_impl_sdlrenderer.cpp",
 
-        projectdir .. "Vendor/imgui/*.cpp",
+        projectdir .. "../Vendor/imgui/*.cpp",
 
         projectdir .. "Source/**.h",
         projectdir .. "Source/**.cpp",
     }
 
     includedirs {
-        "Vendor/SDL2/include",
-        "Vendor/imgui",
-        "Vendor/imgui/backends",
+        "../Vendor/SDL2/include",
+        "../Vendor/imgui",
+        "../Vendor/imgui/backends",
 
         "Source"
     }
 
     libdirs {
-        "Vendor/SDL2/lib/x64",
+        "../Vendor/SDL2/lib/x64",
     }
 
     links {
@@ -41,7 +41,7 @@ project "Prototype"
     }
  
     prebuildcommands {
-        "{COPY} Vendor/SDL2/lib/x64/SDL2.dll %{cfg.targetdir}",
+        "{COPY} ../Vendor/SDL2/lib/x64/SDL2.dll %{cfg.targetdir}",
     }
 
     filter "system:windows"
